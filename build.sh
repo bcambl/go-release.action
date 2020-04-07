25f6c080716d58656d1b764dfbbc6a2dcb4855f7
Blayne Campbell <blayne@blaynecampbell.com>
2020-04-06 20:59:34 -0600

    Update build.sh
    omit symbol and dwarf tables on build

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 set -eux
 
 PROJECT_ROOT="/go/src/github.com/${GITHUB_REPOSITORY}"
+PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
 
 mkdir -p $PROJECT_ROOT
 rmdir $PROJECT_ROOT
 ln -s $GITHUB_WORKSPACE $PROJECT_ROOT
 cd $PROJECT_ROOT
 go get -v ./...
-go build
+go build -v -race -ldflags "-w -s -extldflags '-static'" -o $PROJECT_NAME .
