151bac379bc5ad127493c80bb600723de05b88ed
Blayne Campbell <blayne@blaynecampbell.com>
2020-04-06 21:30:59 -0600

    Update build.sh
    specify build output and file extension

--- build.sh
+++ build.sh
@@ -5,10 +5,15 @@ set -eux
 PROJECT_ROOT="/go/src/github.com/${GITHUB_REPOSITORY}"
 PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
 
+EXT=''
+if [ $GOOS == 'windows' ]; then
+  EXT='.exe'
+fi
+
 mkdir -p $PROJECT_ROOT
 rmdir $PROJECT_ROOT
 ln -s $GITHUB_WORKSPACE $PROJECT_ROOT
 cd $PROJECT_ROOT
 go get -v ./...
-go build -v -ldflags "-w -s -extldflags '-static'" .
+go build -v -ldflags "-w -s -extldflags '-static'" -o $PROJECT_NAME$EXT .
 upx -9 $PROJECT_NAME
